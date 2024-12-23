a51b7e8001c840e7157e64ab5d766fe282e731a7
Creeper50017 <155290281+Creeper50017@users.noreply.github.com>
2024-12-23 23:48:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-
+let coins = 0
+let coinsLS = localStorage.getItem("coins")
+let coinsText = document.getElementById("coinsText")
+function Click(args) {
+  coins++
+  coinsText.textContent = "Монет: "+coins
+  localStorage.setItem("coins",coins)
+}
+if(coinsLS != null) {
+  coins = parseInt(coinsLS)
+}
+function Start() {
+  document.getElementById("Wraith").hidden = false
+}
+coinsText.textContent = "Монет: "+coins
+Telegram.WebApp.onEvent(activated, Start)
